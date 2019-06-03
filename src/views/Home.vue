e88703eba816828dac8299bfd830e6306d15c4cb
Alejandro <icce.redes@gmail.com>
2019-06-03 13:21:50 -0400

    add vue.config.js/ for splash screen iOS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <router-link :to="'/about'">
-      <img src="../assets/logo_transparent_350.png" alt="">
+      <img src="../assets/logo-1.png" alt="logo">
     </router-link>
   </div>
 </template>
